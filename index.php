e8eda0c302c03204e7334271917899cde2765037
BasToTheMax <info@bastothemax.nl>
2025-03-03 11:32:31 +0100

    Force GET requests

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
     define("LOAD", true);
 
+    if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+        http_response_code(405);
+        die("Method not allowed. Please use GET");        
+    }
+
     echo("Hello " . $_SERVER['REQUEST_METHOD'] . " at ". $_SERVER['REQUEST_URI']);
